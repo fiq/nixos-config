fffa89700e59b530fc5e07fd99e30d216c159e28
Raf Gemmail <rafiq@dreamthought.com>
2023-07-09 20:33:36 +1200

    fetchGit does not support sha256

--- hardware-configuration/feynman.nix
+++ hardware-configuration/feynman.nix
@@ -11,7 +11,6 @@
         url = "https://github.com/NixOS/nixos-hardware.git";
         ref = "master";
         rev =  "47dca15d86fdd2eabcf434d7cc0b5baa8d1a463c";
-        sha256 = "1lvwi82658ln08faaaickq0vhvnspazb1ixknsmrxvyb3spvjl9h";
        }}/dell/xps/13-9370"
     ];
 
