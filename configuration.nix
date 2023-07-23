e037c2bf8b9a8f65ab0e3f7a44b77075fcb19228
Raf Gemmail <rafiq@dreamthought.com>
2023-07-23 16:05:25 +1200

    Modified systemd-boot consoleMode

--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,7 @@
   nixpkgs.config.cudaSupport  = false;
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.memtest86.enable = true;
+  boot.loader.systemd-boot.consoleMode = "max";
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   hardware.enableRedistributableFirmware = true;
