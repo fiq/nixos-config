d24c7a85f58ebae565abe6ad8e1eae4b04369cd4
Raf Gemmail <rafiq@dreamthought.com>
2023-08-13 14:03:26 +1200

    enable wifi kernel module

--- configuration.nix
+++ configuration.nix
@@ -23,7 +23,7 @@
   boot.supportedFilesystems = [ "ntfs" ];
 
   # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
