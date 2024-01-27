af6b59a7eb1e6ef85b64e1874d053312e72bf081
Raf G <rafiq@dreamthought.com>
2024-01-27 21:20:48 +1300

    deps - test - duping home manager vscode into global conf

--- configuration.nix
+++ configuration.nix
@@ -188,6 +188,7 @@
     tree
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     vlc
+    vscode
     wget
     wofi
     wl-clipboard
