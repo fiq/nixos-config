628c18709ff9bd41d87352e043dccd13a7960b9d
Raf Gemmail <rafiq@dreamthought.com>
2024-02-25 12:37:18 +1300

    audio - pipewire support for jackd and alsa

--- configuration.nix
+++ configuration.nix
@@ -30,7 +30,18 @@
   # Pick only one of the below networking options.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-  services.pipewire.enable = true;
+
+  # pipewire setup
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    jack.enable = true;
+    alsa.support32Bit = true;
+  };
+  users.extraGroups.audio.members = [ "raf" ];
+  users.extraGroups.realtime.members = [ "raf" ];
+  users.extraGroups.jackaudio.members = [ "raf" ];
+
   # Set your time zone.
   time.timeZone = "Pacific/Auckland";
 
