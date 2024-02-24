b9d8e858b9cedbc5c4ef6f2d46bde7fb75582821
Raf Gemmail <rafiq@dreamthought.com>
2024-02-25 12:13:34 +1300

    config - guitarx and jackd

--- hardware-configuration/hawking.nix
+++ hardware-configuration/hawking.nix
@@ -8,16 +8,14 @@
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
       ./x-android-dev.nix
-#      ./x-guitar.nix
+      ./x-guitar.nix
       ./x-pulseaudio.nix
       ./x-rtl-sdr.nix
       ./x-yubi.nix
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-  #boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" "mt7921e" "snd-seq" "snd-rawmidi" ];
-#  boot.kernelModules = [ "kvm-amd" "mt7921e" ];
+  boot.kernelModules = [ "kvm-amd" "mt7921e" ];
   boot.extraModulePackages = [ ];
   nixpkgs.config.cudaSupport  = true;
 
@@ -88,7 +86,7 @@
   hardware.steam-hardware.enable = true;
 
   # Enable Guitar Kit
-#  services.x-guitar.enable = true;
+  services.x-guitar.enable = true;
 
   # RTL SDR custom module
   services.x-rtl-sdr.enable = true;
@@ -104,62 +102,5 @@
 
   # Allow ports
   networking.firewall.allowedTCPPorts = [ 22 8090 8400 ];
-
-    musnix = {
-      enable = true;
-      # Not using any of Musnix' inbuilt optimisations, because they depend on a
-      # realtime kernel, which is
-      # a) not required for sufficient responsiveness
-      # b) incompatible with running VMs via Virtualbox.
-#      kernel = {
-#        optimize = false;
-#        realtime = false;
-        # Confirm _actual_ latest available kernel at:
-        # https://github.com/musnix/musnix/blob/master/modules/kernel.nix
-        #packages = pkgs.linuxPackages_5_9_rt;
-#      };
-    };
-
-    # Kernel configuration
-    # Do it the hard way, because Musnix assumes the RT kernel is wanted
-    nixpkgs.overlays = [
-      (self: super: {
-        linuxDAW = pkgs.linuxPackagesFor (pkgs.linux_daw.override {
-          structuredExtraConfig = with pkgs.lib.kernel; {
-            PREEMPT = yes;
-            HPET_TIMER = yes;
-          };
-          #ignoreConfigErrors = true;
-        });
-      })
-   ];
-    # As-yet unused, but potentially useful kernel configs:
-    #          #IOSCHED_DEADLINE y
-    #          MQ_IOSCHED_DEADLINE y
-    #          CONFIG_1000_HZ y   # Seems to be obsolete; was explicitly ignored last I tried it.
-
-    #boot.kernelPackages = pkgs.linuxPackages_5_10;
-
-    # Enable user `raf` to use realtime audio
-    users.users.raf.extraGroups = [ "audio" ];
-
-    # Enable the RealtimeKit system service,
-    # which hands out realtime scheduling priority to user processes on demand.
-    security.rtkit.enable = true;
-
-    # Use an appropriate CPU performance governor
-    # Often used values: "ondemand", "powersave", "performance"
-    powerManagement.cpuFreqGovernor = "performance";
-
-    # System limits for Ardour, esp. realtime scheduling.
-    # Seems obsolete as at 2020/03/28, but keeping this here for reference in case I was wrong.
-    #systemd.extraConfig = "DefaultLimitNOFILE=4096\nDefaultLimitMEMLOCK=4G:16G\nDefaultLimitRTPRIO=40";
-    #systemd.extraConfig = "DefaultLimitNOFILE=4096\nDefaultLimitMEMLOCK=24G:16G\nDefaultLimitRTPRIO=40";
-
-    # JACK configuration
-    #boot.kernelModules = [ "snd-seq" "snd-rawmidi" ];
-    hardware.pulseaudio.package =
-      pkgs.pulseaudio.override { jackaudioSupport = true; };
-    hardware.pulseaudio.enable = true;
 
 }
--- hardware-configuration/x-guitar.nix
+++ hardware-configuration/x-guitar.nix
@@ -1,71 +1,59 @@
-{ lib, config, pkgs, ... }:
+{ lib, pkgs, config, musnix, ... }:
 with lib;
-  let cfg = config.services.x-guitar;
+let cfg = config.services;
 in {
   options.services.x-guitar = {
     enable = mkEnableOption "custom guitar setup";
   };
- 
-  config = mkIf cfg.enable {
-	  imports = [
-	    <musnix>                      # Channel
-	    ];
 
-	  musnix = {
-	    enable = true;
-	    # Not using any of Musnix' inbuilt optimisations, because they depend on a
-	    # realtime kernel, which is
-	    # a) not required for sufficient responsiveness
-	    # b) incompatible with running VMs via Virtualbox.
-	    kernel = {
-	      optimize = false;
-	      realtime = false;
-	      # Confirm _actual_ latest available kernel at:
-	      # https://github.com/musnix/musnix/blob/master/modules/kernel.nix
-	      #packages = pkgs.linuxPackages_5_9_rt;
-	    };
-	  };
+  config = mkIf cfg.x-guitar.enable {
+    musnix = { enable = true; };
+    boot.kernelModules = [ "snd-seq" "snd-rawmidi" ];
+    # Kernel configuration
+    # Do it the hard way, because Musnix assumes the RT kernel is wanted
+    nixpkgs.overlays = [
+      (self: super: {
+        linuxDAW = pkgs.linuxPackagesFor (pkgs.linux_daw.override {
+          structuredExtraConfig = with pkgs.lib.kernel; {
+            PREEMPT = yes;
+            HPET_TIMER = yes;
+          };
+          #ignoreConfigErrors = true;
+        });
+      })
+    ];
 
-	  # Kernel configuration
-	  # Do it the hard way, because Musnix assumes the RT kernel is wanted
-	  nixpkgs.overlays = [
-	      (self: super: {
-		linuxDAW = pkgs.linuxPackagesFor (pkgs.linux_daw.override {
-		  structuredExtraConfig = with pkgs.lib.kernel; {
-		    PREEMPT = yes;
-		    HPET_TIMER = yes;
-		  };
-		  #ignoreConfigErrors = true;
-		});
-	      })
-	    ];
-	  # As-yet unused, but potentially useful kernel configs:
-	  #          #IOSCHED_DEADLINE y
-	  #          MQ_IOSCHED_DEADLINE y
-	  #          CONFIG_1000_HZ y   # Seems to be obsolete; was explicitly ignored last I tried it.
+    # jackd
+    services.jack = {
+      jackd.enable = true;
+      alsa.enable = true;
+      #loopback.enable = true;
+    };
 
-	  #boot.kernelPackages = pkgs.linuxPackages_5_10;
+    # Enable user `raf` to use realtime audio
+    users.users.raf.extraGroups = [ "audio" "jackaudio" "realtime" ];
 
-	  # Enable user `raf` to use realtime audio
-	  users.users.raf.extraGroups = [ "audio" ];
+    # Enable the RealtimeKit system service,
+    # which hands out realtime scheduling priority to user processes on demand.
+    security.rtkit.enable = true;
 
+    # Use an appropriate CPU performance governor
+    # Often used values: "ondemand", "powersave", "performance"
+    #powerManagement.cpuFreqGovernor = "performance";
 
-	  # Enable the RealtimeKit system service,
-	  # which hands out realtime scheduling priority to user processes on demand.
-	  security.rtkit.enable = true;
+    # JACK configuration
+    hardware.pulseaudio.package =
+      pkgs.pulseaudio.override { jackaudioSupport = true; };
+    hardware.pulseaudio.enable = true;
+    environment.systemPackages = with pkgs; [
+      libjack2
+      jack2
+      qjackctl
+      jack2Full
+      jack_capture
+      guitarix
+    ];
 
-	  # Use an appropriate CPU performance governor
-	  # Often used values: "ondemand", "powersave", "performance"
-	  powerManagement.cpuFreqGovernor = "performance";
+  };
 
-	  # System limits for Ardour, esp. realtime scheduling.
-	  # Seems obsolete as at 2020/03/28, but keeping this here for reference in case I was wrong.
-	  #systemd.extraConfig = "DefaultLimitNOFILE=4096\nDefaultLimitMEMLOCK=4G:16G\nDefaultLimitRTPRIO=40";
-	  #systemd.extraConfig = "DefaultLimitNOFILE=4096\nDefaultLimitMEMLOCK=24G:16G\nDefaultLimitRTPRIO=40";
-
-	  # JACK configuration
-	  boot.kernelModules=["snd-seq" "snd-rawmidi" ];
-	  hardware.pulseaudio.package = pkgs.pulseaudio.override { jackaudioSupport = true; };
-	  hardware.pulseaudio.enable = true;  
-	};
 }
