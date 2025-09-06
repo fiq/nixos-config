bb23fd3f2a68fa24374dfce50aa752953d1cd4b3
Raf Gemmail <rafiq@dreamthought.com>
2025-09-06 19:19:16 +1200

    fixup - renamed deprecated gnome option

--- configuration.nix
+++ configuration.nix
@@ -92,7 +92,7 @@
 
   # Enable the GNOME Desktop Environment.
   # services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   hardware.graphics.enable = true;
 
